421f42a27fb856bb3988081a001345df5af622ee
Alexander <alexander.zybin@gmail.com>
2015-12-20 23:17:57 +0300

    fkjdhskfdskfh skjdhf ksdhfkds

---
+++ src/block/site/contact/_b-contact/.document-ready.js
@@ -0,0 +1,38 @@
+
+	$('.b-contact').each(function(index){
+		event.preventDefault();
+		
+		var block = $(this);
+		
+		var mMap;
+		
+		function initYandexMap() {
+			
+			if($('#b-contact-map-cont-map').size()) {
+				mMap = new ymaps.Map('b-contact-map-cont-map', {
+					center: [60.949286, 76.61202],
+					zoom: 16,
+				}, {
+					searchControlProvider: 'yandex#search'
+				});
+				mMap.behaviors.disable('scrollZoom');
+				
+				var op_office = new ymaps.Placemark([60.949286, 76.61202], {
+					hintContent: 'Беркан'
+				}, {
+					//iconLayout: 'default#image',
+					//iconImageHref: '/wp-content/themes/solnechny/img/yandex-map-icon.png',
+					//iconImageSize: [104, 115],
+					//iconImageOffset: [-52, -115]
+				});
+				mMap.geoObjects.add(op_office);
+			}
+		
+		}
+		
+		if(typeof ymaps != 'undefined') {
+			ymaps.ready(initYandexMap);
+		}
+		
+	});
+	
