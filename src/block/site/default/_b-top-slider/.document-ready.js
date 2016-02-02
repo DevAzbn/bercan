75ce15ab3afa5211c15fcfdcf5752174d889b242
Alexander <alexander.zybin@gmail.com>
2016-02-02 10:00:51 +0300

    2

---
+++ src/block/site/default/_b-top-slider/.document-ready.js
@@ -0,0 +1,14 @@
+
+	$('.fluid-block').each(function(index){
+		
+		var block = $(this);
+		
+		block.on('click', function(event){
+			event.preventDefault();
+			var btn = $(this);
+			
+			$('.b-top-menu').trigger('hide-drd-menu');
+		})
+		
+	});
+	
